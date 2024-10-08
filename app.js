97089f21ff81e99e81cc8d94bca91a3b30e8961a
burhanfauzisalam <burhanfauzisalam@gmail.com>
2024-10-08 14:28:20 +0700

    08/10/2024

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 2000;
+// Import dotenv
+require("dotenv").config();
 
+const port = process.env.PORT || 2000;
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send(process.env.hello);
 });
 
 app.get("/hello", (req, res) => {
